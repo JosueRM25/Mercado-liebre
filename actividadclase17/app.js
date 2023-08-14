946885e01b2be2c9a29728478d241dbfcf371704
unknown <josuerobertomamani2020@gmail.com>
2023-08-14 11:33:41 -0300

    mercado liebre tremendoo

---
+++ actividadclase17/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+ const app = express();
+ const path = require('path');
+app.use(express.static('public'));
+
+app.get('/',(req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+ 
+ app.listen(3030,() =>{
+    console.log ('[server]: running on part 3030');
+});
+app.get("/register", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+app.get("/login", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
